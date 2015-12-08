0524be265f7762e7beba045df974921918883f56
Daniel Burkhardt <DanBurkhardt@dyn-160-39-162-178.dyn.columbia.edu>
2015-12-08 18:36:11 -0500

    Page updated, images added, new JS

--- LiveSite/scripts/keyRead.js
+++ LiveSite/scripts/keyRead.js
@@ -1,7 +1,8 @@
 // File for loading the local file reading keys into memory
 
 var keyFile = {
-    echoNestKey: "Your key here",
+    echoNestKey: "7IHA9TNCP0DDWSNZW ",
+	echoNestConsumerKey: "1ee6a2b0c0f47aa5c6ced84b921a9484",
     spotifyKey: "Your key here"
 };
 
