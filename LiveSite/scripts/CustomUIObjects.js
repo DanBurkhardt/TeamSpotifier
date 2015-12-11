c11a4cad91cb5ba28f906e8f1e55e89d46fecec7
Dan Burkhardt <danielburkhardt@iMac.local>
2015-12-11 17:09:20 -0500

    updated slider, output, project files:

--- LiveSite/scripts/CustomUIObjects.js
+++ LiveSite/scripts/CustomUIObjects.js
@@ -12,7 +12,26 @@ function durationSlider1Changed(event, ui){
     // Add fuction for refreshing the table with results that match the duration
     
     // Replace the button text with the updated duration
-    $('#u215-4').replaceWith('<div class=\"clearfix grpelem\" id=\"u215-4\"><!-- content --><p>add '+ui.value+' mins of songs to playlist</p></div>');
-    
+    $('#u215-4').replaceWith('<div class=\"clearfix grpelem\" id=\"u215-4\"><!-- content --><p>add '+ui.value+' mins of songs to existing playlist</p></div>'); 
 };
 
+
+
+/*
+/       Scenario 1 - Button Actions
+*/
+function scenario1Listeners(){
+    
+    $( "#buttonu212" ).click(function() {
+        alert( "Handler for button called." );
+        
+            // TODO: Code here for dialogue boxes confirming which list to add to
+    });
+};
+
+
+function addToExisting1(){
+    
+    
+    
+}
